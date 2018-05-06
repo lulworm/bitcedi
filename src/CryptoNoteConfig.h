808c6a70f976d0dd73cbf0749311c48394c6f5f2
cntemple <38593686+cntemple@users.noreply.github.com>
2018-05-06 16:09:10 +0000

    Remove outdated seednodes, adding new ones.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -120,13 +120,9 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "85ae8734f90bc1ee295ceb0ec05a49852d4dbbc9d1c27a619b5f4bdf26a0196e";
 
 const std::initializer_list<const char*> SEED_NODES = {
-   "seed0.bitcedi.org:55008",
-   "seed1.bitcedi.org:55008",
-   "seed2.bitcedi.org:55008",
-   "174.138.3.171:55008",
-   "185.183.99.19:55008", 
-   "138.68.105.149:18080",
-   "172.104.240.101:55008"
+   "173.230.155.185:55008",
+   "172.104.240.101:55008",
+   "45.33.53.50:55008"	   
 };
 
 struct CheckpointData {
