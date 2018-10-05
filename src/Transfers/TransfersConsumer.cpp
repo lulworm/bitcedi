c8e8712fce4a8067e2ffa7386463a1fe082dff79
cntemple <38593686+cntemple@users.noreply.github.com>
2018-10-05 17:48:40 +0000

    Still burning!

--- src/Transfers/TransfersConsumer.cpp
+++ src/Transfers/TransfersConsumer.cpp
@@ -22,7 +22,9 @@
 using namespace Crypto;
 using namespace Logging;
 using namespace Common;
-std::unordered_map<Hash, std::vector<PublicKey>> public_keys_seen;
+std::unordered_set<Crypto::Hash> transactions_hash_seen;
+std::unordered_set<Crypto::PublicKey> public_keys_seen;
+std::mutex seen_mutex;
 
 namespace {
 
@@ -81,7 +83,7 @@ void findMyOutputs(
       for (const auto& key : out.keys) {
         checkOutputKey(derivation, key, idx, idx, spendKeys, outputs);
         ++keyIndex;
-      }
+     }
     }
   }
 }
@@ -356,6 +358,12 @@ void TransfersConsumer::removeUnconfirmedTransaction(const Crypto::Hash& transac
   m_observerManager.notify(&IBlockchainConsumerObserver::onTransactionDeleteEnd, this, transactionHash);
 }
 
+void TransfersConsumer::addPublicKeysSeen(const Crypto::Hash& transactionHash, const Crypto::PublicKey& outputKey) {
+	std::lock_guard<std::mutex> lk(seen_mutex);
+    transactions_hash_seen.insert(transactionHash);
+    public_keys_seen.insert(outputKey);
+}	
+	
 std::error_code createTransfers(
   const AccountKeys& account,
   const TransactionBlockInfo& blockInfo,
@@ -365,7 +373,8 @@ std::error_code createTransfers(
   std::vector<TransactionOutputInformationIn>& transfers) {
 
   auto txPubKey = tx.getTransactionPublicKey();
-  std::vector<PublicKey> temp_keys;	 
+  std::vector<PublicKey> temp_keys;	
+  std::lock_guard<std::mutex> lk(seen_mutex);	
 
   for (auto idx : outputs) {
 
@@ -403,16 +412,14 @@ std::error_code createTransfers(
         info.keyImage);
 
       assert(out.key == reinterpret_cast<const PublicKey&>(in_ephemeral.publicKey));
-	 	  std::unordered_map<Hash, std::vector<PublicKey>>::iterator it = public_keys_seen.find(tx.getTransactionHash());
-	  if (it == public_keys_seen.end()) {
-		  for (const std::pair<Hash, std::vector<PublicKey>>& kv : public_keys_seen) {
-			  auto& keys = kv.second;
-			  if (std::find(keys.begin(), keys.end(), out.key) != keys.end()) {
-				  throw std::runtime_error("duplicate transaction output key is found");
-				  return std::error_code();
-			  }
-		  }
-	     temp_keys.push_back(out.key);		
+	 	        std::unordered_set<Crypto::Hash>::iterator it = transactions_hash_seen.find(tx.getTransactionHash());
+	  if (it == transactions_hash_seen.end()) {
+        std::unordered_set<Crypto::PublicKey>::iterator key_it = public_keys_seen.find(out.key);
+        if (key_it != public_keys_seen.end()) {
+          throw std::runtime_error("duplicate transaction output key is found");
+          return std::error_code();
+        }
+        temp_keys.push_back(out.key);		
 	  }   
 	     
 
@@ -425,14 +432,12 @@ std::error_code createTransfers(
       tx.getOutput(idx, out, amount);
 	    
 	  for (const auto& key : out.keys) {
-		  std::unordered_map<Hash, std::vector<PublicKey>>::iterator it = public_keys_seen.find(tx.getTransactionHash());
-		  if (it == public_keys_seen.end()) {
-			  for (const std::pair<Hash, std::vector<PublicKey>>& kv : public_keys_seen) {
-				  auto& keys = kv.second;
-				  if (std::find(keys.begin(), keys.end(), key) != keys.end()) {
-					  throw std::runtime_error("duplicate transaction output key is found");
-					  return std::error_code();
-				  }
+		  std::unordered_set<Crypto::Hash>::iterator it = transactions_hash_seen.find(tx.getTransactionHash());
+		  if (it == transactions_hash_seen.end()) {
+			  std::unordered_set<Crypto::PublicKey>::iterator key_it = public_keys_seen.find(key);
+			  if (key_it != public_keys_seen.end()) {
+				  throw std::runtime_error("duplicate transaction output key is found");
+				  return std::error_code();
 			  }
  			  std::vector<PublicKey> temp_keys;
 			  temp_keys.push_back(key);
@@ -445,10 +450,14 @@ std::error_code createTransfers(
       info.term = out.term;
     }
     
-    transfers.push_back(info);
+   transfers.push_back(info);
   }
-  public_keys_seen.insert(std::make_pair(tx.getTransactionHash(), temp_keys));  
-	
+
+  transactions_hash_seen.emplace(tx.getTransactionHash());
+  for (std::vector<PublicKey>::iterator it = temp_keys.begin(); it != temp_keys.end(); it++) {
+    public_keys_seen.insert(*it);
+  }
+
   return std::error_code();
 }
 
